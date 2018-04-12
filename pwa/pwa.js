f1ac4b169b42a80e11a3d372e54db7567f259e99
vecchio <weichao@zbj.com>
2018-04-12 18:22:51 +0800

    init pro

---
+++ pwa/pwa.js
@@ -0,0 +1,44 @@
+const cacheStorageKey = 'pwa-demo'
+const cacheList = [
+    "pwa.html",
+    "pwa.css",
+    "favicon.ico"
+]
+
+self.addEventListener('install', e => {
+    console.info('install')
+    e.waitUntil(
+        caches.open(cacheStorageKey)
+        .then(cache => cache.addAll(cacheList))
+        .then(() => self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function(e) {
+    console.info('fetch')
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response != null) {
+                return response
+            }
+            return fetch(e.request.url)
+        })
+    )
+})
+
+self.addEventListener('activate', function(e) {
+    console.info('activate')
+    e.waitUntil(
+        Promise.all(
+            caches.keys().then(cacheNames => {
+                return cacheNames.map(name => {
+                    if (name !== cacheStorageKey) {
+                        return caches.delete(name)
+                    }
+                })
+            })
+        ).then(() => {
+            return self.clients.claim()
+        })
+    )
+})
